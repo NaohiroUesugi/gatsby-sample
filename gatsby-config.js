7efe20fad1df8922f686b4724bb5fe2372a8ad14
上杉　直大 <nhd_8991.4070@icloud.com>
2019-10-20 09:32:32 +0900

    ts styled-component 導入

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,8 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-typescript`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
