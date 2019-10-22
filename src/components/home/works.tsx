c2590552f75493b706bd9f1ab53d10e676afa6ba
上杉　直大 <nhd_8991.4070@icloud.com>
2019-10-23 03:11:28 +0900

    create Work page

--- src/components/home/works.tsx
+++ src/components/home/works.tsx
@@ -1,11 +1,103 @@
 import * as React from 'react'
+import styled from 'styled-components'
+
+import ReactImg from '../../images/react.png'
+import GatsbyImg from '../../images/gatsby.png'
+import Neo4jImg from '../../images/neo4j.png'
+
+const WorksStyle = styled.div`
+  display: grid;
+  grid-template:
+    'header' 300px
+    'main' 2fr
+    / auto;
+`
+const WorksHeder = styled.div`
+  font-family: Verdana;
+  color: #515151;
+  grid-area: header;
+  text-align: center;
+  align-self: center;
+`
+
+const WorksMain = styled.div`
+  margin: 0px 5%;
+  grid-area: main;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 450px));
+  grid-template-rows: 500px;
+  grid-auto-rows: 500px;
+`
+
+const Item = styled.div`
+  display: grid;
+  grid-template:
+    'image' 300px
+    'explanation' 1fr
+    / auto;
+`
+
+const Image = styled.img`
+  /* background: green; */
+  /* text-align:center; */
+  width: 300px;
+  height: 300px;
+  justify-self: center;
+  object-fit: cover;
+`
+
+const Explanation = styled.div`
+  .title{
+    font-weight: bold;
+    font-size: 20px;
+  }
+  text-align:center;
+`
+
+const Button = styled.button`
+  background: palevioletred;
+  color: white;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`
 
 const Works = () => {
   return (
-    <>
-      <h1>WORKS</h1>
-      <p>制作実績</p>
-    </>
+    <WorksStyle>
+      <WorksHeder>
+        <h1>WORKS</h1>
+        <p>制作実績など</p>
+      </WorksHeder>
+      <WorksMain>
+        <Item>
+          <Image src={ReactImg}/>
+          <Explanation>
+            <p className='title'>aaaaLP</p>
+            <p>Coding/Gatsby</p>
+            <Button>Normal</Button>
+          </Explanation>
+        </Item>
+        <Item>
+          <Image src={GatsbyImg}/>
+          <Explanation>
+          <p className='title'>bbbbLP</p>
+          <p>Coding/Gatsby</p>
+          <Button>Normal</Button>
+          </Explanation>
+        </Item>
+        <Item>
+          <Image src={Neo4jImg}/>
+          <Explanation>
+            <p className='title'>ccccLP</p>
+            <p>Coding/Gatsby</p>
+            <Button>Normal</Button>
+          </Explanation>
+        </Item>
+      </WorksMain>
+    </WorksStyle>
   )
 }
 
