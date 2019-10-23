38aef7788958b563091557b944ab3dd9460431ab
上杉　直大 <nhd_8991.4070@icloud.com>
2019-10-24 03:38:11 +0900

    add icon

--- src/components/home/sns_footer.tsx
+++ src/components/home/sns_footer.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react'
 import styled from 'styled-components'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faTwitterSquare, faFacebookSquare, faGithubSquare } from '@fortawesome/free-brands-svg-icons'
+import { faAddressBook, faAngleRight, faChevronCircleUp } from '@fortawesome/free-solid-svg-icons'
+
 const SnsFooterStyle = styled.div`
   height: 400px;
   background: #f7f7f7;
@@ -9,7 +14,12 @@ const SnsFooterStyle = styled.div`
 const SnsFooter = () => {
   return (
     <SnsFooterStyle>
-    <h1>aaaa</h1>
+      <FontAwesomeIcon icon={faTwitterSquare} size="3x" color="pink"/>
+      <FontAwesomeIcon icon={faFacebookSquare} size="4x"/>
+      <FontAwesomeIcon icon={faGithubSquare} size="5x"/>
+      <FontAwesomeIcon icon={faAddressBook} size="3x" color="pink"/>
+      <FontAwesomeIcon icon={faAngleRight} size="4x"/>
+      <FontAwesomeIcon icon={faChevronCircleUp} size="5x"/>
     </SnsFooterStyle>
   )
 }
